c5fe4840f1a45152b93273b348718a7748df6d62
Sergey Larionov <serjlar@arview.pro>
2017-11-10 21:19:14 +0700

    add packages. add first screen.

--- wb/wb/MainActivity.cs
+++ wb/wb/MainActivity.cs
@@ -1,15 +1,17 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
 
 namespace wb
 {
-    [Activity(Label = "wb", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "wb", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/AppTheme.Main")]
+    public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.main);
 
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
